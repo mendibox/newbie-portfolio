71ed5fb99b585c7d1d018a7b50c2f2ef01ab7315
Juan Luis Mendiola <mendibox@gmail.com>
2023-03-15 22:03:53 -0600

    started about section

--- app.js
+++ app.js
@@ -6,30 +6,30 @@ const allSections = document.querySelector(".main-content");
 function pageTransitions() {
   //Button click active class
   for (let i = 0; i < sectBtn.length; i++) {
-    sectBtn[i].addEventListener("click", function () {
-      let currentBtn = document.querySelectorAll(".active-btn");
+    sectBtn[i].addEventListener('click', function () {
+      let currentBtn = document.querySelectorAll('.active-btn');
       currentBtn[0].className = currentBtn[0].className.replace(
-        "active-btn",
-        ""
+        'active-btn',
+        ''
       );
       this.className += " active-btn";
     });
   }
   //Sections active class
-  allSections.addEventListener("click", (e) => {
+  allSections.addEventListener('click', (e) => {
     const id = e.target.dataset.id;
     if (id) {
       //Remove selected from the order buttons
       sectBtns.forEach((btn) => {
-        btn.classList.remove("active");
+        btn.classList.remove('active');
       });
-      e.target.classList.add("active");
+      e.target.classList.add('active');
       //Hide other sections
       sections.forEach((section) => {
-        section.classList.remove("active");
+        section.classList.remove('active');
       });
       const element = document.getElementById(id);
-      element.classList.add("active");
+      element.classList.add('active');
     }
   });
 }
